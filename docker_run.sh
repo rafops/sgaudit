bd16a6fa91f1d62c44a4fdbe535b138d1e569f6c
Rafael Moraes <rafaelm+github@nulogy.com>
2020-12-27 13:17:15 -0500

    Fix input; Add demo

--- docker_run.sh
+++ docker_run.sh
@@ -3,10 +3,8 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPT_PATH"
 
-ACCOUNT_DATA_DIR="$1"
-if [[ -d "${SCRIPT_PATH}/account-data" ]]; then
-  ACCOUNT_DATA_DIR="${SCRIPT_PATH}/account-data"
-fi
+DEFAULT_ACCOUNT_DATA_DIR="${SCRIPT_PATH}/account-data"
+ACCOUNT_DATA_DIR="${1:-$DEFAULT_ACCOUNT_DATA_DIR}"
 
 if [[ ! -d "$ACCOUNT_DATA_DIR" ]]; then
   echo "CloudMapper account-data dir not found"
